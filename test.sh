bd2cff2c3a56e440b7ce3a1dcbc5e1e485f041b3
Denis <carriere.denis@gmail.com>
2020-10-19 09:53:46 -0400

    rename to __tests__

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # compile
-g++ -std=c++11 -Wall -o safemath.t.out safemath.t.cpp -I include
+g++ -std=c++11 -Wall -o safemath.t.out safemath.t.cpp -I __tests__
 
 # test
 ./safemath.t.out --success

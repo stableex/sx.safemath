dd945acdac3cfef65ee2cfefbcf0900d659650f3
YaroShkvorets <shkvorets@gmail.com>
2021-02-15 12:48:09 -0500

    uint128 functions

--- safemath.hpp
+++ safemath.hpp
@@ -52,12 +52,12 @@ namespace safemath {
      * ### example
      *
      * ```c++
-     * const uint128_t z = safemath::mul(2, 2);
+     * const uint64_t z = safemath::mul(2, 2);
      * //=> 4
      * ```
      */
-    static uint128_t mul(const uint64_t x, const uint64_t y) {
-        const uint128_t z = static_cast<uint128_t>(x) * y;
+    static uint64_t mul(const uint64_t x, const uint64_t y) {
+        const uint64_t z = x * y;
         eosio::check(y == 0 || z / y == x, "safemath-mul-overflow"); return z;
     }
 
@@ -77,6 +77,87 @@ namespace safemath {
      * ```
      */
     static uint64_t div(const uint64_t x, const uint64_t y) {
+        eosio::check(y > 0, "safemath-divide-zero");
+        return x / y;
+    }
+
+    /**
+     * ## STATIC `add128`
+     *
+     * ### params
+     *
+     * - `{uint128_t} x`
+     * - `{uint128_t} y`
+     *
+     * ### example
+     *
+     * ```c++
+     * const uint128_t z = safemath::add128(1, 2);
+     * //=> 3
+     * ```
+     */
+    static uint128_t add128( const uint128_t x, const uint128_t y ) {
+        const uint128_t z = x + y;
+        eosio::check( z >= x, "safemath-add-overflow"); return z;
+    }
+
+    /**
+     * ## STATIC `sub128`
+     *
+     * ### params
+     *
+     * - `{uint128_t} x`
+     * - `{uint128_t} y`
+     *
+     * ### example
+     *
+     * ```c++
+     * const uint128_t z = safemath::sub128(3, 2);
+     * //=> 1
+     * ```
+     */
+    static uint128_t sub128(const uint128_t x, const uint128_t y) {
+        const uint128_t z = x - y;
+        eosio::check(z <= x, "safemath-sub-overflow"); return z;
+    }
+
+    /**
+     * ## STATIC `mul128`
+     *
+     * ### params
+     *
+     * - `{uint128_t} x`
+     * - `{uint128_t} y`
+     *
+     * ### example
+     *
+     * ```c++
+     * const uint128_t z = safemath::mul128(2, 2);
+     * //=> 4
+     * ```
+     */
+    static uint128_t mul128(const uint128_t x, const uint128_t y) {
+        const uint128_t z = x * y;
+        eosio::check(y == 0 || z / y == x, "safemath-mul-overflow");
+        return z;
+    }
+
+    /**
+     * ## STATIC `div128`
+     *
+     * ### params
+     *
+     * - `{uint128_t} x`
+     * - `{uint128_t} y`
+     *
+     * ### example
+     *
+     * ```c++
+     * const uint128_t z = safemath::div128(4, 2);
+     * //=> 2
+     * ```
+     */
+    static uint128_t div128(const uint128_t x, const uint128_t y) {
         eosio::check(y > 0, "safemath-divide-zero");
         return x / y;
     }
